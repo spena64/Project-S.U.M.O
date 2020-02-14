4b0811e5a8349a5e6794aff8027e971dbc8ddeb7
Mike Triana <mikejtriana@gmail.com>
2020-02-14 18:14:32 -0500

    Implement match game loop and player movement

--- project/src/match.py
+++ project/src/match.py
@@ -1,3 +1,5 @@
+import time
+
 class Match:
     def __init__(self):
         self.playerDict = {}
@@ -5,20 +7,26 @@ class Match:
     def addPlayer(self, userID, position, accelerationRate, radius, mass):
         self.playerDict[userID] = Player(position, accelerationRate, radius, mass)
 
-    def start(self):
+    def runGameLoop(self):
+        # Game loop
+        while(True):
+            for id in self.playerDict:
+                player = self.playerDict[id]
+                player.move()
         return
 
     def getPlayerLocation(self, userID):
         return self.playerDict[userID].getPosition()
 
-    def setPlayerInput(self, userId, input):
+    def setPlayerInput(self, userId, inputX, inputY):
+        self.playerDict[userId].setDirection(inputX, inputY)
         return
     
 class Player:
     def __init__(self, position, accelerationRate, radius, mass):
         self.position = position
         self.speed = [0, 0]
-        self.acceleration = [0, 0]
+        self.direction = [0, 0]
         self.accelerationRate = accelerationRate
         self.radius = radius
         self.mass = mass
@@ -30,9 +38,16 @@ class Player:
         return self.radius
 
     def move(self):
+        self.speed[0] += self.direction[0] * self.accelerationRate
+        self.speed[1] += self.direction[1] * self.accelerationRate
+
+        self.position[0] += self.speed[0]
+        self.position[1] += self.speed[1]
         return
 
-    def updateDirection(self, playerInput):
+    def setDirection(self, inputX, inputY):
+        self.direction[0] = inputX
+        self.direction[1] = inputY
         return
 
     def checkCollision(self):
--- project/test/test_match.py
+++ project/test/test_match.py
@@ -1,6 +1,7 @@
 import os, sys
 sys.path.append('../src')
 import match
+import threading
 import time
 
 def test_addPlayers1():
@@ -16,7 +17,8 @@ def test_addPlayers2():
 def test_movePlayer1():
     match1 = match.Match()
     match1.addPlayer('0000', [25, 25], 1, 30, 1)
-    match1.start()
-    match1.setPlayerInput('0000', [0, 1])
+    matchThread = threading.Thread(target=match1.runGameLoop, daemon=True)
+    matchThread.start()
+    match1.setPlayerInput('0000', 0, 1)
     time.sleep(0.05)
     assert match1.getPlayerLocation('0000')[1] > 25
