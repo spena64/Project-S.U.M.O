14031983190f917c67059239b11e8720a3057d0f
Steven <stevenifadel@gmail.com>
2020-02-16 17:34:27 -0500

    Change starting html name to index.html for firebase hosting

--- project/temp/index.js
+++ project/temp/index.js
@@ -1,57 +1,63 @@
+
+const email = document.getElementById("email_field");
+const pass = document.getElementById("password_field");
+const cpass = document.getElementById("cpassword");
+const nickname = document.getElementById("nickname_field");
+
+function createAccount()
+{ 
+  var firebaseRef = firebase.database().ref("Users");
   
-  function createAccount()
-  { 
-    var email = document.getElementById("email_field");
-    var pass = document.getElementById("password_field");
-    var cpass = document.getElementById("cpassword");
-    var firebaseRef = firebase.database().ref("Users");
-    
-    if (email.value != "" && pass.value != "" && cpass.value != "")
+  if (email.value != "" && pass.value != "" && cpass.value != "")
+  {
+    var promise = auth.createUserWithEmailAndPassword(email.value, pass.value);
+    promise.catch(e => alert(e.message));
+
+    if (pass.value == cpass.value)
     {
-      if (pass.value == cpass.value)
+      firebaseRef.push(
       {
-        firebaseRef.push(
-        {
-          email: document.getElementById("email_field").value,
-          password: document.getElementById("password_field").value,
-          nickname: document.getElementById("nickname_field").value
-        });
-      
-        const promise = auth.createUserWithEmailAndPassword(email.value, pass.value);
-        promise.catch(e => alert(e.message));
+        email: email.value,
+        password: pass.value,
+        nickname: nickname.value
+      });
 
-        alert("Welcome, " + nickname_field.value + "!");
-        window.location.href = 'login.html';
-      }
-
+      alert("Welcome, " + nickname.value + "!");
+      // NEED TO MAKE IT SO THAT THE BUTTON WILL GO TO LOGIN PAGE AFTERWARDS WILE ALSO SAVING IN THE AUTHENTICATION TOKEN
+      window.location.href = 'login.html';
+    }
       else  
         window.alert("Passwords do not match!");
     }
-
     else
       window.alert("Signup is incomplete!");
   }
 
-    function login()
-    {
-      var email = document.getElementById("email_field");
-      var pass = document.getElementById("password_field");
-      
-      const promise = auth.signInWithEmailAndPassword(email_field.value, password_field.value);
-      promise.catch(e => alert(e.message));
-  
-      alert("Successfully logged in"); 
-      window.location.href = 'home-page.html';
-    }
 
-    function signOut()
-    {
-      auth.signOut();
-      alert("Signed Out");
-    }
+  function login()
+  {   
+    const promise = auth.signInWithEmailAndPassword(email.value, pass.value);
+    promise.catch(e => alert(e.message));
 
-    function continueGuest()
-    {
-      alert("Welcome, " + nickname_field.value + "!!"); 
-      window.location.href = 'home-page.html';
-    }
+    alert("Successfully logged in"); 
+    window.location.href = 'home-page.html';
+  }
+
+  function signOut()
+  {
+    auth.signOut();
+    alert("Signed Out");
+  }
+
+  function continueGuest()
+  {
+    alert("Welcome, " + nickname.value + "!!"); 
+    window.location.href = 'home-page.html';
+  }
+
+  // firebase.auth().onAuthStateChanged(firebaseUser => {
+  //   if(firebaseUser)
+  //     console.log(firebaseUser);
+  //   else
+  //     console.log("not logged in");
+  // });
