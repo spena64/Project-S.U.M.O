43d9fa72aaa0c603bc604349b7230b5466371d82
Steven <stevenifadel@gmail.com>
2020-02-16 20:20:53 -0500

    Add website progress into the src code tab

---
+++ project/src/public/gametest.js
@@ -0,0 +1,89 @@
+// Track user input
+const movement = {
+	up: false,
+	down: false,
+	left: false,
+	right: false
+};
+// Track player locations
+let players = {};
+
+// Listeners for keyboard input
+document.addEventListener('keydown', function(event) {
+	switch (event.keyCode) {
+		case 65: // A
+			movement.left = true;
+			break;
+		case 87: // W
+			movement.up = true;
+			break;
+		case 68: // D
+			movement.right = true;
+			break;
+		case 83: // S
+			movement.down = true;
+			break;
+	}
+});
+document.addEventListener('keyup', function(event) {
+	switch (event.keyCode) {
+		case 65: // A
+			movement.left = false;
+			break;
+		case 87: // W
+			movement.up = false;
+			break;
+		case 68: // D
+			movement.right = false;
+			break;
+		case 83: // S
+			movement.down = false;
+			break;
+	}
+});
+
+// Establish websocket connection
+const ws = new WebSocket("ws://" + location.host + "/game");
+ws.onopen = function() {
+    console.log("WebSocket connected.");
+    var data = {
+        type: "newConnection",
+    }
+    ws.send(data);
+    setInterval(sendInput, 1000 / 60);
+}
+
+// Keep server updated with player input
+sendInput = function() {
+    var data = {
+        type: "gameInput",
+        body: movement,
+    }
+    ws.send(data);
+};
+
+// Receive game state from server
+ws.onmessage = function(event) {
+    var msg = JSON.parse(event.data);
+
+    if (msg.type == "gameState") {
+        players = msg.body;
+    }
+};
+
+
+// Draw game state
+const canvas = document.getElementById('canvas');
+canvas.width = 800;
+canvas.height = 600;
+const context = canvas.getContext('2d');
+setInterval(function() {
+	context.clearRect(0, 0, 800, 600);
+	context.fillStyle = 'green';
+	for (let id in players) {
+		let player = players[id];
+		context.beginPath();
+		context.arc(player.x, player.y, player.radius, 0, 2 * Math.PI);
+		context.fill();
+	}
+}, 1000 / 60);
---
+++ project/src/public/index.js
@@ -0,0 +1,63 @@
+
+const email = document.getElementById("email_field");
+const pass = document.getElementById("password_field");
+const cpass = document.getElementById("cpassword");
+const nickname = document.getElementById("nickname_field");
+
+function createAccount()
+{ 
+  var firebaseRef = firebase.database().ref("Users");
+  
+  if (email.value != "" && pass.value != "" && cpass.value != "")
+  {
+    var promise = auth.createUserWithEmailAndPassword(email.value, pass.value);
+    promise.catch(e => alert(e.message));
+
+    if (pass.value == cpass.value)
+    {
+      firebaseRef.push(
+      {
+        email: email.value,
+        password: pass.value,
+        nickname: nickname.value
+      });
+
+      alert("Welcome, " + nickname.value + "!");
+      // NEED TO MAKE IT SO THAT THE BUTTON WILL GO TO LOGIN PAGE AFTERWARDS WILE ALSO SAVING IN THE AUTHENTICATION TOKEN
+      window.location.href = 'login.html';
+    }
+      else  
+        window.alert("Passwords do not match!");
+    }
+    else
+      window.alert("Signup is incomplete!");
+  }
+
+
+  function login()
+  {   
+    const promise = auth.signInWithEmailAndPassword(email.value, pass.value);
+    promise.catch(e => alert(e.message));
+
+    alert("Successfully logged in"); 
+    window.location.href = 'home-page.html';
+  }
+
+  function signOut()
+  {
+    auth.signOut();
+    alert("Signed Out");
+  }
+
+  function continueGuest()
+  {
+    alert("Welcome, " + nickname.value + "!!"); 
+    window.location.href = 'home-page.html';
+  }
+
+  // firebase.auth().onAuthStateChanged(firebaseUser => {
+  //   if(firebaseUser)
+  //     console.log(firebaseUser);
+  //   else
+  //     console.log("not logged in");
+  // });
