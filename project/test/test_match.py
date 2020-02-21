36ae2f57c81479684896b325daf267617da91c81
Mike Triana <mikejtriana@gmail.com>
2020-02-21 17:24:59 -0500

    Add tests for vector normalization

--- project/test/test_match.py
+++ project/test/test_match.py
@@ -5,20 +5,54 @@ import threading
 import time
 
 def test_addPlayers1():
-    match1 = match.Match()
-    match1.addPlayer('0000', [50, 50], 1, 30, 1)
-    assert match1.getPlayerLocation('0000') == [50, 50]
+    matchObj = match.Match()
+    matchObj.addPlayer('0000', [50, 50], 1, 30, 1)
+    assert matchObj.getPlayerState('0000')["x"]
 
 def test_addPlayers2():
-    match1 = match.Match()
-    match1.addPlayer('0000', [100, 50], 1, 30, 1)
-    assert match1.getPlayerLocation('0000') == [100, 50]
+    matchObj = match.Match()
+    matchObj.addPlayer('0000', [100, 50], 1, 30, 1)
+    assert matchObj.getPlayerState('0000')["x"]
 
 def test_movePlayer1():
-    match1 = match.Match()
-    match1.addPlayer('0000', [25, 25], 1, 30, 1)
-    matchThread = threading.Thread(target=match1.runGameLoop, daemon=True)
+    matchObj = match.Match()
+    matchObj.addPlayer('0000', [25, 25], 1, 30, 1)
+    matchThread = threading.Thread(target=matchObj.runGameLoop, daemon=True)
     matchThread.start()
-    match1.setPlayerInput('0000', 0, 1)
+    matchObj.setPlayerInput('0000', 0, 1)
     time.sleep(0.05)
-    assert match1.getPlayerLocation('0000')[1] > 25
+    assert matchObj.getPlayerState('0000')["y"] > 25
+
+def test_movePlayer2():
+    matchObj = match.Match()
+    matchObj.addPlayer('0000', [25, 25], 1, 30, 1)
+    matchThread = threading.Thread(target=matchObj.runGameLoop, daemon=True)
+    matchThread.start()
+    matchObj.setPlayerInput('0000', -1, -1)
+    time.sleep(0.05)
+    assert matchObj.getPlayerState('0000')["x"] < 25
+    assert matchObj.getPlayerState('0000')["y"] < 25
+
+def test_normalize1():
+    matchObj = match.Match()
+    vector = matchObj.normalizeInput(1, 1)
+    assert vector[0].__round__(3) == 0.707
+    assert vector[1].__round__(3) == 0.707
+
+def test_normalize2():
+    matchObj = match.Match()
+    vector = matchObj.normalizeInput(-1, 1)
+    assert vector[0].__round__(3) == -0.707
+    assert vector[1].__round__(3) == 0.707
+
+def test_normalize3():
+    matchObj = match.Match()
+    vector = matchObj.normalizeInput(2, 3)
+    assert vector[0].__round__(3) == 0.555
+    assert vector[1].__round__(3) == 0.832
+
+def test_normalize4():
+    matchObj = match.Match()
+    vector = matchObj.normalizeInput(0, 0)
+    assert vector[0].__round__(3) == 0.0
+    assert vector[1].__round__(3) == 0.0
