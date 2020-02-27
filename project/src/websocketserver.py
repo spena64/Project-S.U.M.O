fbd337cc8ed69f50cd2365f48f3d02a133cb5216
Mike Triana <mikejtriana@gmail.com>
2020-02-27 18:09:39 -0500

    WIP track connected users, lobbies, and queues

--- project/src/websocketserver.py
+++ project/src/websocketserver.py
@@ -2,62 +2,66 @@ import asyncio
 import json
 import websockets
 import threading
-import match
+from online_user import OnlineUser
+from game_queue import GameQueue
 
 class GameServer:
     def __init__(self):
         self.PORT = 8081
+        self.user_dict = {}
+        self.lobby_dict = {}
 
-        self.test_match = match.Match()
-        self.match_thread = threading.Thread(target=self.test_match.run_game_loop, daemon=True)
-        self.is_match_started = False
-        print("Creating new game server.")
+        self.queue_duo = GameQueue()
 
     async def on_message(self, ws, path):
         user_id = ""
+
         while(True):
             message = await ws.recv()
             message = json.loads(message)
 
             if (message["type"] == "newConnection"):
                 user_id = message["id"]
-                if (self.is_match_started == False):
-                    self.test_match.add_player(user_id, [400, 500], 0.25, 30, 1)
-                    print("New connection at websocket " + str(ws) + " with id " + user_id)
-                    out_msg = {
-                        "type": "info",
-                        "body": "Joined match."
-                    }
-                    await ws.send(json.dumps(out_msg))
+
+                # TODO: Authentiacate user
+
+                print("New connection at websocket " + str(ws) + " with id " + user_id)
+                self.user_dict[user_id] = OnlineUser(user_id)
+                out_msg = {
+                    "type": "info",
+                    "body": "Connected to game server."
+                }
+                await ws.send(json.dumps(out_msg))
+            if (message["type"] == "enqueue"):
+                if (self.user_dict[user_id].status == "idle"):
+                    if (message["body"] == "duo"):
+                        self.queue_duo.add_user(user_id)
+                        self.user_dict[user_id].status = "queueing"
                 else:
                     out_msg = {
                         "type": "info",
-                        "body": "Match already started, cannot join."
+                        "body": "Cannot queue for game, already in a queue or game."
                     }
                     await ws.send(json.dumps(out_msg))
-                    return
-            if (message["type"] == "startMatch" and self.is_match_started == False):
-                self.is_match_started = True
-                print("Started match.")
-                self.match_thread.start()
             if (message["type"] == "gameInput"):
+                # TODO: Find what lobby the player is in
                 player_inputs = message["body"]
                 input_x = player_inputs["right"] - player_inputs["left"]
                 input_y = player_inputs["down"] - player_inputs["up"]
-                self.test_match.set_player_input(user_id, input_x, input_y)
+                # TODO: send inputs to lobby
 
-            # Send player data to client
-            out_data = {
-                "type": "gameState",
-                "matchState": self.test_match.get_match_data(),
-                "body": self.test_match.get_player_data()
-            }
-            if (out_data["matchState"]["winner"] == user_id):
-                out_data["matchState"]["youWin"] = True
-            await ws.send(json.dumps(out_data))
+            if (self.user_dict[user_id].status == "in lobby"):
+                # TODO: get game state
+                out_data = {
+                    "type": "gameState",
+                    "matchState": "",
+                    "body": ""
+                }
+                #if (out_data["matchState"]["winner"] == user_id):
+                    #out_data["matchState"]["youWin"] = True
+                await ws.send(json.dumps(out_data))
             
-            if (out_data["matchState"]["state"] == "finished"):
-                break
+        del self.user_dict[user_id]
 
     def start_game_server(self):
         print("Starting server at port " + str(self.PORT))
