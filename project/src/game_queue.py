b909310b60b213fc0064f811d7a48e1c9fca3374
Mike Triana <mikejtriana@gmail.com>
2020-03-08 16:19:51 -0400

    Skip users in matchmaking queue that have disconnected

--- project/src/game_queue.py
+++ project/src/game_queue.py
@@ -2,16 +2,35 @@ from queue import Queue
 import time
 
 class GameQueue():
-    def __init__(self, g_size):
+    def __init__(self, u_dict, g_size):
         self.user_queue = Queue()
+        self.size = 0
+        self.user_dict = u_dict
         self.group_size = g_size
 
     def add_user(self, user_id):
         self.user_queue.put(user_id)
+        self.size += 1
         print("User " + user_id + " added to queue.")
 
+        print("Queue size: " + str(self.size))
+
     def grab_user(self):
-        return self.user_queue.get()
+        user = self.user_queue.get()
+        self.size -= 1
+
+        print("Queue size: " + str(self.size))
+
+        while (user not in self.user_dict or self.user_dict[user].status != "QUEUEING"):
+            print("Tried to pull invalid user from matchmaking queue.")
+            user = self.user_queue.get()
+
+        return user
+
+    def remove_user(self):
+        self.size -= 1
+
+        print("Queue size: " + str(self.size))
 
     def collect_players(self, num_players):
         group = []
@@ -20,7 +39,7 @@ class GameQueue():
         return group
 
     def find_players(self):
-        while(self.user_queue.qsize() < self.group_size):
+        while(self.size < self.group_size):
             time.sleep(1)
         group = self.collect_players(self.group_size)
         return group
--- project/src/websocketserver.py
+++ project/src/websocketserver.py
@@ -13,7 +13,7 @@ class GameServer:
         self.user_dict = {}
         self.lobby_manager = LobbyManager(self.user_dict)
 
-        self.queue_duo = GameQueue(2)
+        self.queue_duo = GameQueue(self.user_dict, 2)
 
         mm_thread_duo = threading.Thread(target=self.start_matchmaking_duo, daemon=True)
         mm_thread_duo.start()
@@ -22,64 +22,72 @@ class GameServer:
         user_id = "unknown"
 
         while(True):
-            # Process incoming messages
-            message = await ws.recv()
-            message = json.loads(message)
+            try:
+                # Process incoming messages
+                message = await ws.recv()
+                message = json.loads(message)
 
-            if (message["type"] == "newConnection"):
+                if (message["type"] == "newConnection"):
 
-                # TODO: Authentiacate user
+                    # TODO: Authentiacate user
 
-                user_id = message["id"]
-                if (user_id in self.user_dict):
-                    out_msg = {
-                    "type": "info",
-                    "body": "Already serving websocket for this user; closing connection."
-                    }
-                    await ws.send(json.dumps(out_msg))
-                    return
+                    user_id = message["id"]
+                    if (user_id in self.user_dict):
+                        out_msg = {
+                        "type": "info",
+                        "body": "Already serving websocket for this user; closing connection."
+                        }
+                        await ws.send(json.dumps(out_msg))
+                        return
 
-                print("New connection at websocket " + str(ws) + " with id " + user_id)
-                self.user_dict[user_id] = OnlineUser(user_id)
-                out_msg = {
-                    "type": "info",
-                    "body": "Connected to game server."
-                }
-                await ws.send(json.dumps(out_msg))
-            if (message["type"] == "enqueue"):
-                if (self.user_dict[user_id].status == "IDLE"):
-                    if (message["body"] == "duo"):
-                        self.queue_duo.add_user(user_id)
-                        self.user_dict[user_id].status = "QUEUEING"
-                else:
+                    print("New connection at websocket " + str(ws) + " with id " + user_id)
+                    self.user_dict[user_id] = OnlineUser(user_id)
                     out_msg = {
                         "type": "info",
-                        "body": "Cannot queue for game, already in a queue or game."
+                        "body": "Connected to game server."
                     }
                     await ws.send(json.dumps(out_msg))
-            if (message["type"] == "gameInput"):
-                if (self.user_dict[user_id].status == "PLAYING"):
-                    player_inputs = message["body"]
-                    input_x = player_inputs["right"] - player_inputs["left"]
-                    input_y = player_inputs["down"] - player_inputs["up"]
-                    self.lobby_manager.get_lobby_by_playerid(user_id).relay_input(user_id, input_x, input_y)
+                if (message["type"] == "enqueue"):
+                    if (self.user_dict[user_id].status == "IDLE"):
+                        if (message["body"] == "duo"):
+                            self.queue_duo.add_user(user_id)
+                            self.user_dict[user_id].status = "QUEUEING"
+                    else:
+                        out_msg = {
+                            "type": "info",
+                            "body": "Cannot queue for game, already in a queue or game."
+                        }
+                        await ws.send(json.dumps(out_msg))
+                if (message["type"] == "gameInput"):
+                    if (self.user_dict[user_id].status == "PLAYING"):
+                        player_inputs = message["body"]
+                        input_x = player_inputs["right"] - player_inputs["left"]
+                        input_y = player_inputs["down"] - player_inputs["up"]
+                        self.lobby_manager.get_lobby_by_playerid(user_id).relay_input(user_id, input_x, input_y)
 
-            # Send outgoing messages
-            if (self.user_dict[user_id].status == "PLAYING"):
-                lobby = self.lobby_manager.get_lobby_by_playerid(user_id)
-                out_msg = {
-                    "type": "gameState",
-                    "body": lobby.get_game_state(user_id)
-                }
-                await ws.send(json.dumps(out_msg))
-                if (out_msg["body"]["matchData"]["state"] == "finished"):
-                    lobby.remove_player(user_id)
-                    self.user_dict[user_id].status = "IDLE"
-                    self.user_dict[user_id].lobby_id = "none"
-                    break
+                # Send outgoing messages
+                if (self.user_dict[user_id].status == "PLAYING"):
+                    lobby = self.lobby_manager.get_lobby_by_playerid(user_id)
+                    out_msg = {
+                        "type": "gameState",
+                        "body": lobby.get_game_state(user_id)
+                    }
+                    await ws.send(json.dumps(out_msg))
+                    if (out_msg["body"]["matchData"]["state"] == "finished"):
+                        lobby.remove_player(user_id)
+                        self.user_dict[user_id].status = "IDLE"
+                        self.user_dict[user_id].lobby_id = "none"
+                        break
+                
+            except websockets.ConnectionClosed:
+                break
             
-        del self.user_dict[user_id]
+        if (user_id in self.user_dict):
+            if (self.user_dict[user_id].status == "QUEUEING"):
+                self.queue_duo.remove_user()
+            del self.user_dict[user_id]
         print("Connection at websocket " + str(ws) + " closed, user " + user_id + " removed.")
+
 
     def start_matchmaking_duo(self):
         while(True):
